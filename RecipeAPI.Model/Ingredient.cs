58a8d7f1df63f5b5677db0ae1a12038985dba3c1
CzajaKrzysztof <krzysiekczaja@gmail.com>
2019-10-30 11:40:07 +0100

    Add constructor for private setter in IngredientId proporty

--- RecipeAPI.Model/Ingredient.cs
+++ RecipeAPI.Model/Ingredient.cs
@@ -2,10 +2,15 @@
 {
     class Ingredient
     {
-        public int IngredientId { get; set; }
+        public int IngredientId { get; private set; }
         public string IngredientName { get; set; }
         public double Proteins { get; set; }
         public double Carbohydrates { get; set; }
         public double Fat { get; set; }
+
+        public Ingredient(int id)
+        {
+            IngredientId = id;
+        }
     }
 }
