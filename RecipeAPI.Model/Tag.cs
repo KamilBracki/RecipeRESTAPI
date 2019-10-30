a0485ab5e6ef783f31efbf73714c0d36a29f693d
kamilebracki <kamilbracki1@gmail.com>
2019-10-30 11:58:01 +0100

    Add Tag model

---
+++ RecipeAPI.Model/Tag.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecipeAPI.Model
+{
+    class Tag
+    {
+        public int TagId { get; set; }
+        public string TagName { get; set; }
+
+        public Tag(int tagId, string tagName)
+        {
+            TagId = tagId;
+            TagName = tagName;
+        }
+    }
+}
