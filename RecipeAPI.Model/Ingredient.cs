fddc186bda97d6518f2cc9fa216e9bd9b1b41a56
CzajaKrzysztof <krzysiekczaja@gmail.com>
2019-10-30 11:30:00 +0100

    Add Ingredient model

---
+++ RecipeAPI.Model/Ingredient.cs
@@ -0,0 +1,11 @@
+﻿namespace RecipeAPI.Model
+{
+    class Ingredient
+    {
+        public int IngredientId { get; set; }
+        public string IngredientName { get; set; }
+        public double Proteins { get; set; }
+        public double Carbohydrates { get; set; }
+        public double Fat { get; set; }
+    }
+}
