b6bae57eaa27bade5ec157decc10a8bc2d3cc96a
kamilebracki <kamilbracki1@gmail.com>
2019-11-03 09:26:31 +0100

    Add Builder class for Reipe model

--- RecipeAPI.Model/Recipe.cs
+++ RecipeAPI.Model/Recipe.cs
@@ -32,7 +32,80 @@ namespace RecipeAPI.Model
             Categories = builder.Categories;
 
         }
-        
+        public class Builder
+        {
+            public int RecipeId;
+            public string Name;
+            public int PreparationTime;
+            public string Complexity;
+            public string LinkToPhoto;
+            public string MethodOfPreparation;
+            public double Kcal;
+            public Dictionary<int, double> Ingredients;
+            public List<Tag> Tags;
+            public List<Category> Categories;
+
+
+            public Builder WithId(int id)
+            {
+                RecipeId = id;
+                return this;
+            }
+            public Builder WithName(string name)
+            {
+                Name = name;
+                return this;
+            }
+
+            public Builder WithPreparationTie(int prepTime)
+            {
+                PreparationTime = prepTime;
+                return this;
+            }
+
+            public Builder WithComplexity(string complexity)
+            {
+                Complexity = complexity;
+                return this;
+            }
+
+            public Builder WithLinkToPhoto(string linkToPhoto)
+            {
+                LinkToPhoto = linkToPhoto;
+                return this;
+            }
+
+            public Builder WithMethodOfPreparation(string methodOfPrep)
+            {
+                MethodOfPreparation = methodOfPrep;
+                return this;
+            }
+            public Builder WithKcal(double kcal)
+            {
+                Kcal = kcal;
+                return this;
+            }
+            public Builder WithIngredients(Dictionary<int, double> ingredients)
+            {
+                Ingredients = ingredients;
+                return this;
+            }
+            public Builder WithTags(List<Tag> tags)
+            {
+                Tags = tags;
+                return this;
+            }
+            public Builder WithCategorise(List<Category> categories)
+            {
+                Categories = categories;
+                return this;
+            }
+
+            public Recipe Build()
+            {
+                return new Recipe(this);
+            }
+        }
 
     }
 
