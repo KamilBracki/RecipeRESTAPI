6a8e02c74c48ac5c9744fed57a41492401dcbf92
kamilebracki <kamilbracki1@gmail.com>
2019-11-03 09:29:40 +0100

    Add builder class for Ingredient model

--- RecipeAPI.Model/Ingredient.cs
+++ RecipeAPI.Model/Ingredient.cs
@@ -15,9 +15,53 @@
             IngredientId = builder.IngredientId;
             IngredientName = builder.IngredientName;
             Proteins = builder.Proteins;
-            Carbohydrates = builder.Proteins;
+            Carbohydrates = builder.Carbohydrates;
             Fat = builder.Fat;
         }
-        
+        public class Builder
+        {
+            public int IngredientId;
+            public string IngredientName;
+            public double Proteins;
+            public double Carbohydrates;
+            public double Fat;
+
+
+
+            public Builder WithIngredientId(int id)
+            {
+                IngredientId = id;
+                return this;
+            }
+            public Builder WithName(string name)
+            {
+                IngredientName = name;
+                return this;
+            }
+
+            public Builder WithProteins(int proteins)
+            {
+                Proteins = proteins;
+                return this;
+            }
+
+            public Builder Withcarbohydrates(int carboydrates)
+            {
+                Carbohydrates = carboydrates;
+                return this;
+            }
+
+            public Builder WithFat(int fats)
+            {
+                Fat = fats;
+                return this;
+            }
+            
+
+            public Ingredient Build()
+            {
+                return new Ingredient(this);
+            }
+        }
     }
 }
