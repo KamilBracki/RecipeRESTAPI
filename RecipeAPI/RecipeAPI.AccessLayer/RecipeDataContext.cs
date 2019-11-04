a0b2176e32293c0e5aa6e87429ecdcce0a77950e
kamilebracki <kamilbracki1@gmail.com>
2019-11-04 11:28:57 +0100

    Add initial DB contet class

---
+++ RecipeAPI/RecipeAPI.AccessLayer/RecipeDataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecipeAPI.AccessLayer
+{
+    class RecipesDataContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+
+}
