b74e3e5760e50ccbad44015548695542dc3f5c38
kamilebracki <kamilbracki1@gmail.com>
2019-11-04 11:02:30 +0100

    Add initial Entity project

---
+++ RecipeAPI/RecipeAPI.AccessLayer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RecipeAPI.AccessLayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
