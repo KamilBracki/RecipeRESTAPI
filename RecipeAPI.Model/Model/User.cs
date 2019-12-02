13cdd26e72af5e4182bc1ae307eb9f68da4fd8ed
Rafal <r4fgacon@gmail.com>
2019-12-02 10:39:17 +0100

    Add user models

---
+++ RecipeAPI.Model/Model/User.cs
@@ -0,0 +1,42 @@
+﻿namespace RecipeAPI.Model.Model
+{ 
+
+    public class User
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+        public Permission Permission { get; set; }
+
+
+
+        public User(int id, string name, string surname, string password, string email, Permission permission)
+        {
+            Id = id;
+            Name = name;
+            Surname = surname;
+            Password = password;
+            Email = email;
+            Permission = permission;
+        }
+
+        public User()
+        {
+        }
+
+        public enum Permission1
+        {
+            USER = 1,
+            MOD = 1 << 1,
+            ADMIN = 1 << 2
+        }
+    }
+}
